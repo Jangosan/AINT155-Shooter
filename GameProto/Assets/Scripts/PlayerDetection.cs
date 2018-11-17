b8b4ff69213fbf7471ef4d886c9617420db3404a
Jangosan <43547237+Jangosan@users.noreply.github.com>
2018-11-17 23:33:18 +0000

    done work on detection and prototype level design

--- GameProto/Assets/Scripts/PlayerDetection.cs
+++ GameProto/Assets/Scripts/PlayerDetection.cs
@@ -13,16 +13,47 @@ public class PlayerDetection : MonoBehaviour {
     //Assigns the values for the two above variables
 	void Start () {
         player = GameObject.FindGameObjectWithTag("Player");
-        parentCharacter = gameObject.transform.parent.gameObject;
+
+        if(gameObject.transform.parent != null)
+        {
+            parentCharacter = gameObject.transform.parent.gameObject;
+        }
+        
 
     }
 
     //When the collider is entered, if the target is the player, the script sends a message to the parent to run the AssignTarget method with the player as the target
     private void OnTriggerEnter2D(Collider2D target)
     {
-        if (target.transform == player.transform)
+        if (target.transform == player.transform && parentCharacter != null)
         {         
             parentCharacter.SendMessage("AssignTarget", target.transform, SendMessageOptions.DontRequireReceiver);
+        }
+    }
+
+    //For event dependent target assigning instead of based off the detection range
+    public void assignTargetOnEvent()
+    {      
+            gameObject.SendMessage("AssignTarget", player.transform, SendMessageOptions.DontRequireReceiver);       
+    }
+
+
+    //For use with spawners. When children get damaged the spawner sends a message to the children to target the player
+    public void assignTargetForChildren()
+    {
+        InvokeRepeating("loopChildTargetAssign", 0, Time.deltaTime);
+    }
+
+    private void loopChildTargetAssign()
+    {
+        for (int i = 0; i < gameObject.transform.childCount; i++)
+        {
+            if (gameObject.transform.GetChild(i).tag == "Enemy")
+            {
+                print(gameObject.transform.GetChild(i).name);
+                gameObject.transform.GetChild(i).SendMessage("AssignTarget", player.transform);
+            }
+
         }
     }
 }
